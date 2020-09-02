d5d506d2270b4e51b5f90450afc7db7a259fbdac
katjacodes <katjacodes@gmail.com>
2020-09-01 20:47:15 -0700

    JavaScript experiment featuring some CSS animation

---
+++ #day41/JS/Early-JavaScript-Experiments.js
@@ -0,0 +1,9 @@
+function message1(msg1)
+	{
+  		document.getElementById('output1').innerHTML = msg1;
+	}
+
+function message2(msg2)
+	{
+  		document.getElementById('output2').innerHTML = msg2;
+	}
