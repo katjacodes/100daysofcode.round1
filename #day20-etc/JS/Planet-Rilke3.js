2dd21bd99225405766442da4ab2ec8749f46ed84
katjacodes <katjacodes@gmail.com>
2020-09-14 20:24:46 -0700

    Beginning to experiment with some very basic JavaScript for my poetry website

---
+++ #day20-etc/JS/Planet-Rilke3.js
@@ -0,0 +1,9 @@
+var answer = prompt("Do you have a favorite poem?")
+
+if (answer == "yes") {
+		document.write("What's its title?");
+
+} 
+  
+else
+  document.write("Maybe you'll find one on this site.");
