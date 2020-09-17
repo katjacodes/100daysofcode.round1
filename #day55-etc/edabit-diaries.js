ec87897ff4c575d810e5d03766fe7971f5946eb1
katjacodes <katjacodes@gmail.com>
2020-09-16 18:48:54 -0700

    A place to chronicle my JavaScript practice on edabit

---
+++ #day55-etc/edabit-diaries.js
@@ -0,0 +1,18 @@
+/*day 55
+Perimeter of a Rectange*/
+function findPerimeter(length, width) {
+	return 2 * length + 2 * width;
+}
+
+/*Area of a Triangle*/
+function triArea(base, height) {
+	return 0.5 * base * height;
+	
+}
+
+/*A String Including "something," a space, and a particular variable*/
+function giveMeSomething(a) {
+	return "something" + " " + a;
+	
+}
+
