9faa6dd9270854bc20b9f2d8774e93a6c05c23d0
katjacodes <katjacodes@gmail.com>
2020-09-21 21:04:11 -0700

    Added additional code to the Edabit Diaries

--- #day55-etc/edabit-diaries.js
+++ #day55-etc/edabit-diaries.js
@@ -6,13 +6,19 @@ function findPerimeter(length, width) {
 
 /*Area of a Triangle*/
 function triArea(base, height) {
-	return 0.5 * base * height;
-	
+	return 0.5 * base * height;	
 }
 
 /*A String Including "something," a space, and a particular variable*/
 function giveMeSomething(a) {
 	return "something" + " " + a;
-	
 }
 
+/*A Function That Returns "True" When Two Numbers Are Equal and Otherwise Returns "False"*/
+function isSameNum(num1, num2) {
+	if (num1 === num2) {
+		return true;
+	} else {
+			return false;
+	}
+}
