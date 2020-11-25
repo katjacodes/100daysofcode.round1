858f67bbf5244229878be08a218a37b258a69274
katjacodes <katjacodes@gmail.com>
2020-11-25 22:39:06 +0100

    More challenges involving hashes in Ruby

--- #day82-etc/aaOpen-introToProgramming.rb
+++ #day82-etc/aaOpen-introToProgramming.rb
@@ -1057,3 +1057,24 @@ puts hand_score("AQAJ") #=> 11
 puts hand_score("jJka") #=> 9
 
 
+# Write a method frequent_letters that takes in a string and returns an array containing the characters that appeared more than twice in the string.
+
+def frequent_letters(string)
+  count = Hash.new(0)
+  string.each_char { |char| count[char] += 1 }
+
+  frequents = []
+  count.each do |char, num|
+    if num > 2
+      frequents << char
+    end
+  end
+  return frequents
+end
+
+print frequent_letters('mississippi') #=> ["i", "s"]
+puts
+print frequent_letters('bootcamp') #=> []
+puts
+
+
