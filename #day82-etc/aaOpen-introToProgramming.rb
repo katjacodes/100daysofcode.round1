29a4b310e3ef43d1027071696377ab648ffeb9d8
katjacodes <katjacodes@gmail.com>
2020-11-26 13:33:22 +0100

    Final batch of RRuby challenges

--- #day82-etc/aaOpen-introToProgramming.rb
+++ #day82-etc/aaOpen-introToProgramming.rb
@@ -1078,3 +1078,36 @@ print frequent_letters('bootcamp') #=> []
 puts
 
 
+# Write a method hash_to_pairs that takes in a hash and returns a 2D array representing each key-value pair of the hash.
+
+def hash_to_pairs(hash)
+  a = hash.keys
+  b = hash.values
+  
+  print a.zip(b)
+end
+
+print hash_to_pairs({"name"=>"skateboard", "wheels"=>4, "weight"=>"7.5 lbs"}) #=> [["name", "skateboard"], ["wheels", 4], ["weight", "7.5 lbs"]]
+puts
+
+
+print hash_to_pairs({"kingdom"=>"animalia", "genus"=>"canis", "breed"=>"German Shepherd"}) #=> [["kingdom", "animalia"], ["genus", "canis"], ["breed", "German Shepherd"]]
+puts
+
+# OR
+
+def hash_to_pairs(hash)
+  pairs = []
+  hash.each { |key, val| pairs << [key, val] }
+  return pairs
+end
+
+print hash_to_pairs({"name"=>"skateboard", "wheels"=>4, "weight"=>"7.5 lbs"}) #=> [["name", "skateboard"], ["wheels", 4], ["weight", "7.5 lbs"]]
+puts
+
+print hash_to_pairs({"kingdom"=>"animalia", "genus"=>"canis", "breed"=>"German Shepherd"}) #=> [["kingdom", "animalia"], ["genus", "canis"], ["breed", "German Shepherd"]]
+puts
+
+
+
+
